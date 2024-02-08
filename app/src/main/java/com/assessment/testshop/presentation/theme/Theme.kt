ab80dc182f05b5a7c0fce1d9539458e6c9d23382
HardcatAntoine <br41nk1ck3r@gmail.com>
2024-02-08 12:44:51 +0300

    theme change

--- app/src/main/java/com/assessment/testshop/presentation/theme/Theme.kt
+++ app/src/main/java/com/assessment/testshop/presentation/theme/Theme.kt
@@ -38,7 +38,7 @@ private val LightColorScheme = lightColorScheme(
 
 @Composable
 fun TestShopTheme(
-    darkTheme: Boolean = true,
+    darkTheme: Boolean = false,
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
