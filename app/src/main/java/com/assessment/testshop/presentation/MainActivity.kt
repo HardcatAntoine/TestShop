1eaa6e7b79d4d8af03078954859f1db33b7b9e1a
HardcatAntoine <br41nk1ck3r@gmail.com>
2024-02-12 18:38:05 +0300

    add TopBar with ProductDetailScreen

--- app/src/main/java/com/assessment/testshop/presentation/MainActivity.kt
+++ app/src/main/java/com/assessment/testshop/presentation/MainActivity.kt
@@ -7,21 +7,28 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.assessment.testshop.R
 import com.assessment.testshop.presentation.components.TestShopBottomTabRow
+import com.assessment.testshop.presentation.navigation.CatalogDestination
 import com.assessment.testshop.presentation.navigation.Route
 import com.assessment.testshop.presentation.navigation.SignUpDestination
 import com.assessment.testshop.presentation.navigation.TestShopNavHost
 import com.assessment.testshop.presentation.navigation.navigateSingleTopTo
 import com.assessment.testshop.presentation.navigation.testShopBottomNavScreens
+import com.assessment.testshop.presentation.screen_cart.ShoppingCartScreen
 import com.assessment.testshop.presentation.theme.TestShopTheme
 import com.assessment.testshop.presentation.utils.resolveTitle
 import dagger.hilt.android.AndroidEntryPoint
@@ -50,10 +57,37 @@ fun TestShopApp() {
         Scaffold(
             topBar = {
                 currentDestination?.route?.let {
-                    CenterAlignedTopAppBar(
-                        modifier = Modifier.height(60.dp),
-                        title = { Text(modifier = Modifier.padding(top = 16.dp), text = resolveTitle(it), fontSize = 20.sp) }
-                    )
+                    if (it.contains(Route.PRODUCT_DETAILS.name)) {
+                        TopAppBar(
+                            title = { },
+                            navigationIcon = {
+                                IconButton(onClick = { navController.navigate(CatalogDestination.route) }) {
+                                    Icon(
+                                        painter = painterResource(id = R.drawable.ic_back),
+                                        contentDescription = "Back"
+                                    )
+                                }
+                            }, actions = {
+                                IconButton(onClick = { }) {
+                                    Icon(
+                                        painter = painterResource(id = R.drawable.ic_share),
+                                        contentDescription = "Share"
+                                    )
+                                }
+                            })
+                    } else {
+                        CenterAlignedTopAppBar(
+                            modifier = Modifier.height(60.dp),
+                            title = {
+                                Text(
+                                    modifier = Modifier.padding(top = 16.dp),
+                                    text = resolveTitle(it),
+                                    fontSize = 20.sp
+                                )
+                            }
+                        )
+                    }
+
                 }
             },
             bottomBar = {
