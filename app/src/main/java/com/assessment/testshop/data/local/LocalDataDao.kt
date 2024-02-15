da685b97167348991356ed934d9f42930d6722ce
HardcatAntoine <br41nk1ck3r@gmail.com>
2024-02-15 11:13:45 +0300

    add dao

---
+++ app/src/main/java/com/assessment/testshop/data/local/LocalDataDao.kt
@@ -0,0 +1,23 @@
+package com.assessment.testshop.data.local
+
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+interface LocalDataDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertPerson(person: Person)
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertFavoriteProduct(favoriteProduct: FavoriteProduct)
+
+    @Delete
+    suspend fun removeFavoriteProduct(favoriteProduct: FavoriteProduct)
+
+    @Query("SELECT*FROM person")
+    suspend fun getPerson(): Person
+
+    @Query("SELECT*FROM `favorite products`")
+    suspend fun getFavoriteProducts(): List<FavoriteProduct>
+}
