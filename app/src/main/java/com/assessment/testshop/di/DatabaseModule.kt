c9b426d2ff825c37f085460cd280d1bac91285bf
HardcatAntoine <br41nk1ck3r@gmail.com>
2024-02-15 11:14:49 +0300

    add Hilt DatabaseModule

---
+++ app/src/main/java/com/assessment/testshop/di/DatabaseModule.kt
@@ -0,0 +1,36 @@
+package com.assessment.testshop.di
+
+import android.app.Application
+import androidx.room.Room
+import com.assessment.testshop.data.LocalDataRepository
+import com.assessment.testshop.data.local.LocalDataDao
+import com.assessment.testshop.data.local.LocalDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+    @Provides
+    @Singleton
+    fun provideDatabase(application: Application): LocalDatabase {
+        return Room.databaseBuilder(
+            application,
+            LocalDatabase::class.java,
+            "database"
+        ).build()
+    }
+
+    @Provides
+    fun providesLocalDataDao(database: LocalDatabase): LocalDataDao {
+        return database.localDataDao()
+    }
+
+    @Provides
+    fun providesLocalDataRepository(dao: LocalDataDao): LocalDataRepository {
+        return LocalDataRepository(dao)
+    }
+}
