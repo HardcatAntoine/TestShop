9f2b847f14194374fbb45bb2f016a4d424417288
HardcatAntoine <br41nk1ck3r@gmail.com>
2024-02-15 11:13:57 +0300

    add database

---
+++ app/src/main/java/com/assessment/testshop/data/local/LocalDatabase.kt
@@ -0,0 +1,9 @@
+package com.assessment.testshop.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [Person::class], [FavoriteProduct::class], version = 1)
+abstract class LocalDatabase : RoomDatabase() {
+    abstract fun localDataDao(): LocalDataDao
+}
