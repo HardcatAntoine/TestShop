d31896fb80e8730adf885f4e42f9f1335cacff7f
HardcatAntoine <br41nk1ck3r@gmail.com>
2024-02-16 16:38:24 +0300

    create GetFavoriteProductsCatalogUseCase

---
+++ app/src/main/java/com/assessment/testshop/domain/GetFavoriteProductsCatalogUseCase.kt
@@ -0,0 +1,25 @@
+package com.assessment.testshop.domain
+
+import com.assessment.testshop.data.ProductsRepository
+import com.assessment.testshop.domain.models.Product
+import com.assessment.testshop.domain.models.toProduct
+import javax.inject.Inject
+
+class GetFavoriteProductsCatalogUseCase @Inject constructor(
+    private val productsRepository: ProductsRepository
+) {
+    suspend operator fun invoke(userId: String = USER_ID): List<Product> {
+        val dtoProducts = productsRepository.getProducts(userId).items
+        val products = dtoProducts.map { it.toProduct() }
+        val favoriteIds = productsRepository.getFavoriteProductIds()
+
+        return products.map { product ->
+            if (favoriteIds.any { it.id == product.id }) {
+                product.copy(isFavorite = true)
+            } else {
+                product
+            }
+        }.filter { it.isFavorite }
+
+    }
+}
